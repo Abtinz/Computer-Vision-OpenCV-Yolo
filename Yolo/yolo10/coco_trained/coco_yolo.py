a4b3efd566fc48e8c2df1c4b8f9cc49aa30c2086
Abtinz <abtinzandi@gmail.com>
2024-07-16 22:58:17 +0330

    results

--- Yolo/yolo10/coco_trained/coco_yolo.py
+++ Yolo/yolo10/coco_trained/coco_yolo.py
@@ -6,10 +6,10 @@ print("yoloV10 before training on coco8",model)
 model.train(data="coco8.yaml", epochs=100, imgsz=640)
 print("yoloV10 after training on coco8",model)
 
-results = model('Yolo\yolo10\image.jpg')
+results = model('Yolo\yolo10\coco_trained\image.jpg')
 print(results)
 results[0].show()
 
-results2 = model('Yolo\yolo10\image2.jpg')
+results2 = model('Yolo\yolo10\coco_trained\image2.jpg')
 print(results2)
 results2[0].show()
